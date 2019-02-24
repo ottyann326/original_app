9d94af589227fae53f6b0a79847044a724b46d07
ottyann326 <ottyann326@gmail.com>
2019-02-24 14:25:14 +0900

    編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,15 @@ class UsersController < ApplicationController
     user.destroy
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :age)
